30782595e933e9e0f68677485d2062287205989f
Abel Surreira <surreira@gmail.com>
2022-06-18 09:18:04 +0000

    feat: implement layouts and initial code clean up

---
+++ components/header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => {
+  return <div className="fixed top-0 z-10 w-full h-5 bg-green-400" />;
+};
+
+export default Header;
---
+++ components/layout.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import { ReactNode } from "react";
+import Header from "./header";
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div style={{ WebkitTapHighlightColor: "transparent" }}>
+      <Head>
+        <title>Receitas TM</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🍳</text></svg>"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover, user-scalable=no"
+        />
+      </Head>
+
+      <Header />
+
+      {children}
+    </div>
+  );
+};
