5e147bb263c0f61cea2763de307f45862924897f
Abel Surreira <surreira@gmail.com>
2022-06-21 17:17:26 +0000

    feat: implement endpoint to index sanity data in algolia

---
+++ pages/api/search/index-all.ts
@@ -0,0 +1,62 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { algoliaIndex } from "../../../lib/algolia";
+import sanity from "../../../lib/sanity";
+
+type Data = {
+  message: string;
+};
+
+const QUERY: string = `
+  * [_type == "recipe" && !(_id in path("drafts.**")) && defined(slug.current)]{
+    _type,
+    _rev,
+    title,
+    time,
+    "objectID": _id,
+    ingredients,
+    "mainImageRef": photo.asset._ref,
+    "steps": steps[tip == false].text,
+    "category": category->{
+      _id,
+      title
+    },
+    "accessories": accessories[]->{
+      _id,
+      title
+    }
+  }
+`;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== "POST") {
+    return res.status(400).json({ message: "Must be POST request." });
+  }
+
+  if (req.headers["x-authentication"] !== process.env.ALGOLIA_WEBHOOK_SECRET) {
+    return res.status(401).json({ message: "Unauthorized." });
+  }
+
+  let recipes;
+  try {
+    recipes = await sanity.fetch(QUERY);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unable to fetch Sanity data to index." });
+    return;
+  }
+
+  try {
+    if (recipes) {
+      await algoliaIndex.saveObjects(recipes);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Unable to index data in Algolia." });
+    return;
+  }
+
+  res.json({ message: "Indexing success" });
+}
